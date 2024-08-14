6bbbfa6b6d87e35c3b6ef83968924ac2da0f1ff6
ENStudio <esi.enstudio@gmail.com>
2024-08-14 13:13:00 +0600

    Modify navigation links

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,20 @@ export default {
 
     plugins: [
         forms,
-        require('flowbite/plugin')
+        require('flowbite/plugin'),
+        function ({addUtilities})
+        {
+            const newUtilities = {
+                ".no-scrollbar::-webkit-scrollbar": {
+                    display: "none",
+                },
+                "no-scrollbar": {
+                    "-ms-overflow-style": "none",
+                    "scrollbar-width": "none",
+                },
+            };
+
+            addUtilities(newUtilities);
+        }
     ],
 };
