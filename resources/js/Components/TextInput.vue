b5bd804962555bad1d96318ec83d84262c5cefca
Emil <nilemil007@gmail.com>
2024-11-04 16:56:13 +0600

    TextInput component added

---
+++ resources/js/Components/TextInput.vue
@@ -0,0 +1,26 @@
+<script setup>
+const model = defineModel({
+    type: null,
+    required: true,
+});
+
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        default: 'text',
+    },
+    message: String,
+});
+</script>
+
+<template>
+    <div class="mb-5">
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{name}}</label>
+        <input v-model="model" :type="type" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        <small v-if="message" class="text-red-500 font-semibold">{{ message }}</small>
+    </div>
+</template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -6,38 +6,19 @@
 
         <form @submit.prevent="submit" class="max-w-md mt-5">
             <!-- Full Name -->
-            <div class="mb-5">
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
-                <input v-model="form.name" type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                <small v-if="form.errors.name" class="text-red-500 font-semibold">{{ form.errors.name }}</small>
-            </div>
+            <TextInput name="Full Name" v-model="form.name" :message="form.errors.name"/>
 
             <!-- Phone Number -->
-            <div class="mb-5">
-                <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
-                <input v-model="form.phone" type="text" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                <small v-if="form.errors.phone" class="text-red-500 font-semibold">{{ form.errors.phone }}</small>
-            </div>
+            <TextInput name="Phone Number" v-model="form.phone" :message="form.errors.phone"/>
 
             <!-- Email -->
-            <div class="mb-5">
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                <input v-model="form.email" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                <small v-if="form.errors.email" class="text-red-500 font-semibold">{{ form.errors.email }}</small>
-            </div>
+            <TextInput name="Email" v-model="form.email" :message="form.errors.email"/>
 
             <!-- Password -->
-            <div class="mb-5">
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input v-model="form.password" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                <small v-if="form.errors.password" class="text-red-500 font-semibold">{{ form.errors.password }}</small>
-            </div>
+            <TextInput type="password" name="Password" v-model="form.password" :message="form.errors.password"/>
 
             <!-- Confirm Password -->
-            <div class="mb-5">
-                <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-                <input v-model="form.password_confirmation" type="password" id="password_confirmation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
+            <TextInput type="password" name="Confirm Password" v-model="form.password_confirmation"/>
 
             <button type="submit" :class="form.processing ? 'cursor-not-allowed opacity-50' : ''" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" :disabled="form.processing">Register</button>
         </form>
@@ -48,6 +29,7 @@
 <script setup>
 import Authenticate from "@/Layouts/Authenticate.vue";
 import { useForm } from "@inertiajs/vue3";
+import TextInput from "@/Components/TextInput.vue";
 
 
 const form = useForm({
